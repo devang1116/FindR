ecb487b588ca93cbf78dbbe0ab87ad2a024ab5a4
Devang <papinwardevang@gmail.com>
2022-01-01 12:36:26 +0530

    Initial Commit

---
+++ FindR/FindRApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindRApp.swift
+//  FindR
+//
+//  Created by Devang Papinwar on 01/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FindRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
